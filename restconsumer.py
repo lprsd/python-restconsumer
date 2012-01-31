8d7b64a681c577b75bc422268f71fdd2b13b05f8
Lakshman Prasad <lakshman@uswaretech.com>
2012-01-31 15:00:16 +0530

    Changed tabs to spaces (damn, sublime default) and did removed the self mutating anti-pattern.

--- restconsumer.py
+++ restconsumer.py
@@ -2,57 +2,51 @@ import requests, json
 
 class RestConsumer(object):
 
-	def __init__(self,base_url,append_json=False,**kwargs):
-		self.base_url = base_url
-		self.uriparts = []
-		self.append_json = append_json
-		self.kwargs = kwargs
+    def __init__(self,base_url,append_json=False,**kwargs):
+        self.base_url = base_url
+        self.uriparts = kwargs.pop('uriparts',[])
+        self.append_json = append_json
+        for k,v in kwargs.items():
+            setattr(self,k,v)
 
-	def __getattr__(self,k):
-		if k=='spc':
-			return self.spc
-		if not k[0] == '_':
-			self.uriparts.append(k)
-		else:
-			if k[1:3]=='in':
-				self.uriparts.append(k[3:])
-			elif k[1:3] == 'uh':
-				self.uriparts.append(k.replace('_','-'))
-		return self
+    def __getattr__(self,key):
+        self.uriparts.append(key)
+        return self.__class__(**self.__dict__)        
 
-	def spc(self,k):
-		self.uriparts.append(k)
-		return self
+    def spc(self,key):
+        return self.__getattr__(key)
 
-	def __call__(self, **kwargs):
-		uri_constructed = '/'.join(self.uriparts)
-		self.uriparts = []
-		self.uri_constructed = "%s%s"%(uri_constructed,'.json') if self.append_json else uri_constructed
-		self.url = '/'.join([self.base_url,self.uri_constructed])
-		return self.get(self.url,**kwargs)
+    def __getitem__(self,key):
+        return self.__getattr__(key)
 
-	def get(self,url,**kwargs):
-		print url
-		r = requests.get(url,**kwargs)
-		return json.loads(r.content)
+    def __call__(self, **kwargs):
+        uri_constructed = '/'.join(self.uriparts)
+        self.uriparts = []
+        self.uri_constructed = "%s%s"%(uri_constructed,'.json') if self.append_json else uri_constructed
+        self.url = '/'.join([self.base_url,self.uri_constructed])
+        return self.get(self.url,**kwargs)
 
-	def post(self,**kwargs):
-		r = requests.post(**kwargs)
-		return json.loads(r.content)
+    def get(self,url,**kwargs):
+        r = requests.get(url,**kwargs)
+        return json.loads(r.content)
+
+    def post(self,**kwargs):
+        r = requests.post(**kwargs)
+        return json.loads(r.content)
 
 if __name__=='__main__':
-	from pprint import pprint
-	t = RestConsumer(base_url='https://api.twitter.com/1',append_json=True)
-	public_timeline = t.statuses.public_timeline()
-	pprint(public_timeline)
+    from pprint import pprint
+    t = RestConsumer(base_url='https://api.twitter.com/1',append_json=True)
+    public_timeline = t.statuses.public_timeline()
+    pprint(public_timeline)
 
-	g = RestConsumer(base_url='https://api.github.com')
-	repos = g.users.kennethreitz.repos()
-	pprint(repos)
+    g = RestConsumer(base_url='https://api.github.com')
+    repos = g.users.kennethreitz.repos()
+    pprint(repos)
 
-	s = RestConsumer(base_url='http://api.stackoverflow.com/1.1')
-	sr = s.users.spc('55562').questions.unanswered()
-	pprint(sr)
+    s = RestConsumer(base_url='http://api.stackoverflow.com/1.1')
+    sr = s.users['55562'].questions.unanswered()
+    pprint(sr)
 
-	sr2 = s.tags.python.spc('top-answerers').spc('all-time')()
-	pprint(sr2)
+    sr2 = s.tags.python['top-answerers']['all-time']
+    pprint(sr2())
