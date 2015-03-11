d0450fc6c08baf196081d7f9fd88b53e3ff4a8b3
murchik <mixturchik@gmail.com>
2015-03-11 14:49:35 +0800

    Tiny refactoring, request_kwargs constructor param added.

--- restconsumer.py
+++ restconsumer.py
@@ -1,37 +1,55 @@
-import requests, json
+import json
+import requests
 
-def append_to_url(base_url,param):
-    return "%s%s/" % (base_url,param)
+
+def append_to_url(base_url, param):
+    return "{}{}/".format(base_url, param)
 
 
 class RestConsumer(object):
+    """ Call REST-like endpoints this way:
 
-    def __init__(self,base_url,append_json=False,append_slash=False):
-        self.base_url = base_url if base_url[-1] == '/' else "%s%s" % (base_url,"/")
-        self.append_json = append_json
-        self.append_slash = append_slash
+        >>> consumer = RestConsumer('http://example.com/api/v1/'
+        >>> response = consumer.projects.groups[22].events()
 
-    def __getattr__(self,key):
-        new_base = append_to_url(self.base_url,key)
+        and it will call http://example.com/api/v1/projects/groups/22/events/
+    """
+
+    base_url = ''
+
+    _append_json = False
+    _append_slash = False
+    _requests_kwargs = {}
+
+    def __init__(self, base_url, append_json=False, append_slash=False, requests_kwargs={}):
+        self.base_url = base_url if base_url[-1] == '/' else "{}{}".format(base_url, "/")
+        self._append_json = append_json
+        self._append_slash = append_slash
+        self._requests_kwargs = requests_kwargs
+
+    def __getattr__(self, key):
+        new_base = append_to_url(self.base_url, key)
         return self.__class__(base_url=new_base,
-                              append_json=self.append_json,
-                              append_slash=self.append_slash)
-    
-    def __getitem__(self,key):
+                              append_json=self._append_json,
+                              append_slash=self._append_slash,
+                              requests_kwargs=self._requests_kwargs)
+
+    def __getitem__(self, key):
         return self.__getattr__(key)
 
     def __call__(self, **kwargs):
-        if not self.append_slash:
+        if not self._append_slash:
             self.base_url = self.base_url[:-1]
-        if self.append_json:
-            self.base_url = "%s%s" % (self.base_url,'.json')
-        print "Calling %s" % self.base_url
-        return self.get(self.base_url,**kwargs)
+        if self._append_json:
+            self.base_url = "{}{}".format(self.base_url, '.json')
+        if self._requests_kwargs:
+            kwargs.update(self._requests_kwargs)
+        return self.get(self.base_url, **kwargs)
 
-    def get(self,url,**kwargs):
-        r = requests.get(url,**kwargs)
+    def get(self, url, **kwargs):
+        r = requests.get(url, **kwargs)
         return json.loads(r.content)
 
-    def post(self,**kwargs):
+    def post(self, **kwargs):
         r = requests.post(**kwargs)
         return json.loads(r.content)
