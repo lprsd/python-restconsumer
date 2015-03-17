e59bf970d9502fbc65879990b2ead85070aaf43c
murchik <mixturchik@gmail.com>
2015-03-17 17:21:04 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    'requests',
+]
+
+setup(
+    name='python-restconsumer',
+    version='1.1.0',
+    description='RESTful API generic client with sweet interface.',
+    long_description='See README.rst',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    author='murchik',
+    author_email='mixturchik@gmail.com',
+    url='https://github.com/moorchegue/python-restconsumer',
+    keywords='rest api generic client',
+    packages=find_packages(),
+    py_modules=['restconsumer'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
